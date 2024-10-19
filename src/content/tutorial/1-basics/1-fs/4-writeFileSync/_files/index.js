17c5012a8468d6fb952be4a3197d580e3985ad9c
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-10-19 22:27:30 +0200

    chore: scheme

--- src/content/tutorial/1-basics/1-fs/4-writeFileSync/_files/index.js
+++ src/content/tutorial/1-basics/1-fs/4-writeFileSync/_files/index.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'node:fs';
 
 const NAME_OF_THE_FILE;
 const FILE_CONTENT;
--- src/content/tutorial/1-basics/1-fs/4-writeFileSync/_solution/index.js
+++ src/content/tutorial/1-basics/1-fs/4-writeFileSync/_solution/index.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'node:fs';
 
 const NAME_OF_THE_FILE = 'file.txt';
 const FILE_CONTENT = 'Hey, here is your content.';
